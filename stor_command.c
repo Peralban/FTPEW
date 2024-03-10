70b9bf7e72c4a92903f47bb129a34b581d3860c7
PerAlban <alban.peralta@epitech.eu>
2024-03-10 01:47:49 +0100

    :sparkles: Create Stor function

--- stor_command.c
+++ stor_command.c
@@ -8,6 +8,61 @@
 #include "include/server.h"
 #include "include/return_error_code.h"
 #include "include/check_param.h"
+#include <fcntl.h>
+
+static void do_stor_command(int accept_socket, client_t *client,
+    char *file_name)
+{
+    int fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    char buffer[1024];
+    int size = 1;
+
+    dprintf(client->socket, return_error(C150, NULL));
+    if (fd == -1) {
+        dprintf(client->socket, return_error(E550, NULL));
+        return;
+    }
+    for (; size > 0; size = read(accept_socket, buffer, 1024)) {
+        buffer[size] = '\0';
+        dprintf(fd, "%s", buffer);
+    }
+    close(fd);
+    close(accept_socket);
+    dprintf(client->socket, return_error(C226, NULL));
+    client->mode = UNKNOW;
+    exit(0);
+}
+
+static void stor_command_passive(client_t *client, char *file_name)
+{
+    int pid;
+    int accept_socket = accept(client->clientServer->socket, NULL, NULL);
+
+    check_return_value(accept_socket, ACCEPT);
+    pid = fork();
+    check_return_value(pid, FORK);
+    if (pid == 0)
+        do_stor_command(accept_socket, client, file_name);
+    close(accept_socket);
+    close(client->clientServer->socket);
+}
+
+static void stor_command_active(client_t *client, server_t *serv
+    __attribute__((unused)), char *file_name)
+{
+    int pid;
+    socklen_t size = sizeof(client->clientServer->serverAddress);
+    int accept_socket = connect(client->clientServer->socket,
+    (struct sockaddr *)client->clientServer->serverAddress, size);
+
+    check_return_value(accept_socket, CONNECT);
+    pid = fork();
+    check_return_value(pid, FORK);
+    if (pid == 0)
+        do_stor_command(accept_socket, client, file_name);
+    close(accept_socket);
+    close(client->clientServer->socket);
+}
 
 void stor_command(client_t *client, char **command, server_t *serv
     __attribute__((unused)), client_list_t **client_list
@@ -17,7 +72,7 @@ void stor_command(client_t *client, char **command, server_t *serv
         dprintf(client->socket, return_error(E530, NULL));
         return;
     }
-    if (check_param(command[1], client->socket, EXIST) == 0) {
+    if (!check_param(command[1], client->socket, EXIST)) {
         dprintf(client->socket, return_error(E501, NULL));
         return;
     }
@@ -25,4 +80,8 @@ void stor_command(client_t *client, char **command, server_t *serv
         dprintf(client->socket, return_error(E425, NULL));
         return;
     }
+    if (client->mode == PASSIVE)
+        stor_command_passive(client, command[1]);
+    else if (client->mode == ACTIVE)
+        stor_command_active(client, serv, command[1]);
 }
